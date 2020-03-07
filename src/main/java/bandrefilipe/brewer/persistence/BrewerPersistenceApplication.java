9a41db24e0e05d2e10c85818d1f13b501b4cd3a4
Andre Barranco <b.andrefilipe@gmail.com>
2020-03-07 16:02:22 -0300

    create brewer persistence project

---
+++ src/main/java/bandrefilipe/brewer/persistence/BrewerPersistenceApplication.java
@@ -0,0 +1,12 @@
+package bandrefilipe.brewer.persistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewerPersistenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrewerPersistenceApplication.class, args);
+    }
+}
---
+++ src/test/java/bandrefilipe/brewer/persistence/BrewerPersistenceApplicationTests.java
@@ -0,0 +1,12 @@
+package bandrefilipe.brewer.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewerPersistenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
