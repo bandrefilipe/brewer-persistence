8baf0b760e2ec5585d01c8a8438586963bc3e90e
Andre Barranco <b.andrefilipe@gmail.com>
2020-03-24 23:33:00 -0300

    prototype repositories

---
+++ src/main/java/bandrefilipe/brewer/persistence/repository/BeverageRepository.java
@@ -0,0 +1,13 @@
+package bandrefilipe.brewer.persistence.repository;
+
+import bandrefilipe.brewer.persistence.model.Beverage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BeverageRepository extends JpaRepository<Beverage, Long> {
+
+    Optional<Beverage> findBySkuIgnoreCase(String sku);
+}
---
+++ src/main/java/bandrefilipe/brewer/persistence/repository/BeverageTypeRepository.java
@@ -0,0 +1,8 @@
+package bandrefilipe.brewer.persistence.repository;
+
+import bandrefilipe.brewer.persistence.model.BeverageType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BeverageTypeRepository extends JpaRepository<BeverageType, Long> {}
