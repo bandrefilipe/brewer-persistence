6798b94cf3be5b459a7429de534bce9c0006d0ac
Andre Barranco <b.andrefilipe@gmail.com>
2020-03-24 23:13:00 -0300

    add swagger documentation

--- src/main/java/bandrefilipe/brewer/persistence/BrewerPersistenceApplication.java
+++ src/main/java/bandrefilipe/brewer/persistence/BrewerPersistenceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BrewerPersistenceApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(BrewerPersistenceApplication.class, args);
     }
 }
---
+++ src/main/java/bandrefilipe/brewer/persistence/config/SwaggerConfig.java
@@ -0,0 +1,68 @@
+package bandrefilipe.brewer.persistence.config;
+
+import bandrefilipe.brewer.persistence.controller.ControllerPackageMarker;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+
+    @Value("${swagger.api.info.title}")
+    private String title;
+
+    @Value("${swagger.api.info.version}")
+    private String version;
+
+    @Value("${swagger.api.info.description:}")
+    private String description;
+
+    @Value("${swagger.api.info.contact.name:}")
+    private String contactName;
+
+    @Value("${swagger.api.info.contact.url:}")
+    private String contactUrl;
+
+    @Value("${swagger.api.info.contact.email:}")
+    private String contactEmail;
+
+    @Value("${swagger.api.info.license:}")
+    private String license;
+
+    @Value("${swagger.api.info.license-url:}")
+    private String licenseUrl;
+
+    @Bean
+    Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(ControllerPackageMarker.class.getPackageName()))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(this.apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(title)
+                .version(version)
+                .description(description)
+                .contact(this.contact())
+                .license(license)
+                .licenseUrl(licenseUrl)
+                .build();
+    }
+
+    private Contact contact() {
+        return new Contact(contactName, contactUrl, contactEmail);
+    }
+}
